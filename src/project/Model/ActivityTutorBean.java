b43037478a690181fccb7e6df08751cf4435a5ab
Francesco-Pagano <frank.pagano@protonmail.ch>
2020-01-14 00:57:39 +0100

    ActivityBean javadoc

--- src/project/Model/ActivityTutorBean.java
+++ src/project/Model/ActivityTutorBean.java
@@ -3,6 +3,10 @@ package project.Model;
 import java.io.Serializable;
 import java.sql.Date;
 
+/**
+ * Questa classe rappresenta l'entità Attività.
+ * @author 
+ */
 public class ActivityTutorBean implements Serializable {
 	private static final long serialVersionUID = 8213234379677788321L;
 	
@@ -24,6 +28,18 @@ public class ActivityTutorBean implements Serializable {
 		hours = -1.f;
 	}
 	
+	/**
+	 * @param idActivity identificativo dell'attività
+	 * @param startTime tempo di inzio dell'attività
+	 * @param finishTime tempo di fine dell'attività
+	 * @param registerId identificativo del registro
+	 * @param category categoria dell'attività
+	 * @param state stato dell'attività
+	 * @param details dettagli dell'attività
+	 * @param tutor tutor che si occupa dell'attività
+	 * @param activityDate data dell'attività
+	 * @param hours ore dell'attività
+	 */
 	public ActivityTutorBean(int idActivity, int startTime, int finishTime, int registerId, String category, String state, String details, String tutor, Date activityDate, float hours) {
 		this.idActivity=idActivity;
 		this.startTime=startTime;
@@ -37,91 +53,152 @@ public class ActivityTutorBean implements Serializable {
 		this.hours=hours;
 	}
 
+	/**
+	 * @return idActivity identificativo dell'attività
+	 */
 	public int getIdActivity() {
 		return idActivity;
 	}
 
+	/**
+	 * @param idActivity idActivity identificativo dell'attività da assegnare all'attività
+	 * @return
+	 */
 	public void setIdActivity(int idActivity) {
 		this.idActivity = idActivity;
 	}
 
-	
+	/**
+	 * @return startTime tempo di inizio dell'attività
+	 */
 	public int getStartTime() {
 		return startTime;
 	}
 
+	/**
+	 * @param startTime startTIme tempo di inizio da assegnare all'attività
+	 * @return
+	 */
 	public void setStartTime(int startTime) {
 		this.startTime = startTime;
 	}
 
-	
+	/**
+	 * @return finishTime tempo di fine dell'attività
+	 */
 	public int getFinishTime() {
 		return finishTime;
 	}
 
+	/**
+	 * @param finishTime finishTime tempo di fine da assegnare all'attività
+	 * @return
+	 */
 	public void setFinishTime(int finishTime) {
 		this.finishTime = finishTime;
 	}
 
-	
+	/**
+	 * @return registerId identificativo del registro dell'attività
+	 */
 	public int getRegisterId() {
 		return registerId;
 	}
 
+	/**
+	 * @param registerId registerId identificativo del registro da assegnare all'attività
+	 * @return
+	 */
 	public void setRegisterId(int registerId) {
 		this.registerId = registerId;
 	}
 
-	
+	/**
+	 * @return category categoria dell'attività
+	 */
 	public String getCategory() {
 		return category;
 	}
 
+	/**
+	 * @param category category categoria da assegnare all'attività
+	 * @return
+	 */
 	public void setCategory(String category) {
 		this.category = category;
 	}
 
-	
+	/**
+	 * @return state stato dell'attività
+	 */
 	public String getState() {
 		return state;
 	}
 
+	/**
+	 * @param state state stato da assegnare all'attività
+	 * @return
+	 */
 	public void setState(String state) {
 		this.state = state;
 	}
 
-	
+	/**
+	 * @return details dettagli dell'attività
+	 */
 	public String getDetails() {
 		return details;
 	}
 
+	/**
+	 * @param details details dettagli da assegnare all'attività
+	 * @return
+	 */
 	public void setDetails(String details) {
 		this.details = details;
 	}
 
-	
+	/**
+	 * @return tutor tutor che si occupa dell'attività
+	 */
 	public String getTutor() {
 		return tutor;
 	}
 
+	/**
+	 * @param tutor tutor tutor da assegnare all'attività
+	 * @return
+	 */
 	public void setTutor(String tutor) {
 		this.tutor = tutor;
 	}
 
-	
+	/**
+	 * @return activityDate data dell'attività
+	 */
 	public Date getActivityDate() {
 		return activityDate;
 	}
-
+	
+	/**
+	 * @param activityDate activityDate data da assegnare all'attività
+	 * @return
+	 */
 	public void setActivityDate(Date activityDate) {
 		this.activityDate = activityDate;
 	}
 
-	
+	/**
+	 * @return hours ore dell'attività
+	 */
 	public float getHours() {
 		return hours;
 	}
 
+	/**
+	 * @param hours hours ore da assegnare all'attività
+	 * @return
+	 */
 	public void setHours(float hours) {
 		this.hours = hours;
 	}
