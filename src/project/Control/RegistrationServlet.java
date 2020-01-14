429af220281a472f76fcb058b88b5e3b190fd49d
Manuel Pisciotta <m.pisciotta8@studenti.unisa.it>
2020-01-14 16:38:14 +0100

    Inizio test registrazione

--- src/project/Control/RegistrationServlet.java
+++ src/project/Control/RegistrationServlet.java
@@ -57,7 +57,7 @@ public class RegistrationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		boolean ajax = Boolean.parseBoolean(request.getParameter("ajax"));  // Richieste ajax per validazione email
 				
 		if(ajax) {
--- src/project/Test/RegistrationTest.java
+++ src/project/Test/RegistrationTest.java
@@ -1,4 +1,4 @@
-/*package project.Test;
+package project.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -112,7 +112,8 @@ public class RegistrationTest {
 
 		assertEquals(message, response.getContentAsString());
 	}
-
+}
+/*
 
 	//TC_2.0_6 Formato nome non corretto
 	@Test
